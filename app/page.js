cbbba3fd063d87c15d7850700ca0cec84c4fce7d
Narayan <88489426+narayan4256k@users.noreply.github.com>
2025-10-05 20:01:07 +0530

    started cration of homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@stackframe/stack";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>hello</h1>
+      <Link href={'/dashboard'}>
       <Button>Hi</Button>
+      </Link>
       <UserButton/>
     </div>
   );
